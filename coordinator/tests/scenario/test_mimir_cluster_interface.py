aeb28baa014e6f1f90b8c1c3cf7822d18b41f229
Jose C. Massón <jose.masson@canonical.com>
2024-06-12 16:11:16 -0300

    fix scenarios test

--- coordinator/tests/scenario/test_mimir_cluster_interface.py
+++ coordinator/tests/scenario/test_mimir_cluster_interface.py
@@ -2,11 +2,11 @@ from itertools import chain
 
 import ops
 import pytest
-from mimir_cluster import (
-    MimirClusterProvider,
-    MimirClusterRequirerAppData,
-    MimirClusterRequirerUnitData,
-    MimirRole,
+from loki_cluster import (
+    LokiClusterProvider,
+    LokiClusterRequirerAppData,
+    LokiClusterRequirerUnitData,
+    LokiRole,
 )
 from ops import Framework
 from scenario import Context, Relation, State
@@ -15,44 +15,44 @@ from scenario import Context, Relation, State
 class MyCharm(ops.CharmBase):
     META = {
         "name": "lukasz",
-        "requires": {"mimir-cluster-require": {"interface": "mimir_cluster"}},
-        "provides": {"mimir-cluster-provide": {"interface": "mimir_cluster"}},
+        "requires": {"loki-cluster-require": {"interface": "loki_cluster"}},
+        "provides": {"loki-cluster-provide": {"interface": "loki_cluster"}},
     }
 
     def __init__(self, framework: Framework):
         super().__init__(framework)
-        self.provider = MimirClusterProvider(self, endpoint="mimir-cluster-provide")
+        self.provider = LokiClusterProvider(self, endpoint="loki-cluster-provide")
 
 
 @pytest.mark.parametrize(
     "workers_roles, expected",
     (
         (
-            (({MimirRole.overrides_exporter}, 1), ({MimirRole.overrides_exporter}, 1)),
-            ({MimirRole.overrides_exporter: 2}),
+            (({LokiRole.read}, 1), ({LokiRole.read}, 1)),
+            ({LokiRole.read: 2}),
         ),
         (
-            (({MimirRole.query_frontend}, 1), ({MimirRole.overrides_exporter}, 1)),
-            ({MimirRole.overrides_exporter: 1, MimirRole.query_frontend: 1}),
+            (({LokiRole.write}, 1), ({LokiRole.read}, 1)),
+            ({LokiRole.read: 1, LokiRole.write: 1}),
         ),
-        ((({MimirRole.querier}, 2), ({MimirRole.querier}, 1)), ({MimirRole.querier: 3})),
+        ((({LokiRole.backend}, 2), ({LokiRole.backend}, 1)), ({LokiRole.backend: 3})),
         (
             (
-                ({MimirRole.alertmanager}, 2),
-                ({MimirRole.alertmanager}, 2),
-                ({MimirRole.alertmanager, MimirRole.querier}, 1),
+                ({LokiRole.read}, 2),
+                ({LokiRole.read}, 2),
+                ({LokiRole.read, LokiRole.backend}, 1),
             ),
-            ({MimirRole.alertmanager: 5, MimirRole.querier: 1}),
+            ({LokiRole.read: 5, LokiRole.backend: 1}),
         ),
     ),
 )
 def test_role_collection(workers_roles, expected):
     relations = []
     for worker_roles, scale in workers_roles:
-        data = MimirClusterRequirerAppData(roles=worker_roles).dump()
+        data = LokiClusterRequirerAppData(roles=worker_roles).dump()
         relations.append(
             Relation(
-                "mimir-cluster-provide",
+                "loki-cluster-provide",
                 remote_app_data=data,
                 remote_units_data={i: {} for i in range(scale)},
             )
@@ -82,15 +82,15 @@ def test_role_collection(workers_roles, expected):
 def test_address_collection(workers_addresses):
     relations = []
     topo = {"unit": "foo/0", "model": "bar"}
-    remote_app_data = MimirClusterRequirerAppData(roles=[MimirRole.alertmanager]).dump()
+    remote_app_data = LokiClusterRequirerAppData(roles=[LokiRole.read]).dump()
     for worker_addresses in workers_addresses:
         units_data = {
-            i: MimirClusterRequirerUnitData(address=address, juju_topology=topo).dump()
+            i: LokiClusterRequirerUnitData(address=address, juju_topology=topo).dump()
             for i, address in enumerate(worker_addresses)
         }
         relations.append(
             Relation(
-                "mimir-cluster-provide",
+                "loki-cluster-provide",
                 remote_units_data=units_data,
                 remote_app_data=remote_app_data,
             )
