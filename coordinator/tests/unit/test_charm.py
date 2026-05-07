4d7070cc878b3d7fc7e6029db3986ef4568f52a9
Michael Thamm <mike.thamm@canonical.com>
2026-05-07 11:23:29 -0400

    feat: Coordinated workers package v3 (#62)

--- coordinator/tests/unit/test_charm.py
+++ coordinator/tests/unit/test_charm.py
@@ -11,7 +11,6 @@ from scenario import Container, Exec, Relation, State
 from src.loki_config import (
     LOKI_ROLES_CONFIG,
     MINIMAL_DEPLOYMENT,
-    RECOMMENDED_DEPLOYMENT,
 )
 
 
@@ -23,7 +22,6 @@ from src.loki_config import (
         ({"distributor": 1}, False),
         ({"distributor": 1, "ingester": 1}, False),
         (dict.fromkeys(MINIMAL_DEPLOYMENT, 1), True),
-        (RECOMMENDED_DEPLOYMENT, True),
     ),
 )
 def test_coherent(mock_coordinator, roles, expected):
@@ -35,28 +33,6 @@ def test_coherent(mock_coordinator, roles, expected):
     mc._roles_config = LOKI_ROLES_CONFIG
 
     assert mc.is_coherent is expected
-
-
-@patch("coordinated_workers.coordinator.Coordinator.__init__", return_value=None)
-@pytest.mark.parametrize(
-    "roles, expected",
-    (
-        ({"query-frontend": 1}, False),
-        ({"distributor": 1}, False),
-        ({"distributor": 1, "ingester": 1}, False),
-        (dict.fromkeys(MINIMAL_DEPLOYMENT, 1), False),
-        (RECOMMENDED_DEPLOYMENT, True),
-    ),
-)
-def test_recommended(mock_coordinator, roles, expected):
-    mc = Coordinator(None, None, "", "", 0, None, None, None)  # type: ignore
-    cluster_mock = MagicMock()
-    cluster_mock.gather_roles = MagicMock(return_value=roles)
-    mc.cluster = cluster_mock
-    mc._override_recommended_checker = None
-    mc._roles_config = LOKI_ROLES_CONFIG
-
-    assert mc.is_recommended is expected
 
 @pytest.mark.parametrize(
     "set_config",
