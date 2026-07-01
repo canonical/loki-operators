5c69289907ab0c26a934c14cb1455743143910c3
Sina P <55766091+sinapah@users.noreply.github.com>
2026-07-01 12:50:28 -0400

    feat(tf): base input variable (#140)

--- worker/terraform/main.tf
+++ worker/terraform/main.tf
@@ -9,6 +9,7 @@ resource "juju_application" "loki_worker" {
   units              = var.units
 
   charm {
+    base     = var.base
     name     = "loki-worker-k8s"
     channel  = var.channel
     revision = var.revision
